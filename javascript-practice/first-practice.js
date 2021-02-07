d63a3af5144a06482b8411de4abbf0d4edc29344
Natalle <natallemoura7@gmail.com>
2021-02-07 18:57:16 +0100

    Start using js again!

---
+++ javascript-practice/first-practice.js
@@ -0,0 +1,13 @@
+console.log("Remembering how to use bash and node");
+
+var number;
+number = 14;
+
+var remainder = number % 3;
+console.log(remainder);
+
+var newNumber = 5;
+newNumber += 5;
+newNumber++;
+
+console.log(newNumber);
